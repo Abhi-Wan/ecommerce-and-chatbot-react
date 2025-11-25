e011111cb1828f51a221c5094c885448849196b1
Abhinav <Abhinav.42.Singh@gmail.com>
2025-11-24 18:30:55 -0800

    Add the starting files

---
+++ chatbot-project/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+import { ChatInput } from './components/ChatInput'
+import { Chatbot } from 'supersimpledev';
+import ChatMessages from './components/ChatMessages';
+import './App.css'
+
+function App() {
+  const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('messages')) || []);
+
+  useEffect(() => {
+    Chatbot.addResponses({
+      "Clanker": "Hey now, there's no need for such language :("
+    })
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('messages', JSON.stringify(chatMessages));
+  }, [chatMessages]);
+
+  return (
+    <div className="app-container">
+      {chatMessages.length === 0 && (
+        <p className="welcome-message" >
+          Welcome to the chatbot project! Send a message using the textbox below.
+        </p>
+      )}
+      <ChatMessages
+        chatMessages={chatMessages}
+      />
+      <ChatInput
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+    </div>
+  );
+}
+
+export default App
---
+++ chatbot-project/src/components/ChatMessages.jsx
@@ -0,0 +1,26 @@
+import { useAutoScroll } from "../hooks/useAutoScroll";
+import { ChatMessage } from "./ChatMessage";
+import './ChatMessages.css'
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useAutoScroll([chatMessages]);
+
+  return (
+    <div 
+      className="chat-messages-container"
+      ref={chatMessagesRef}>
+        {chatMessages.map((chatMessage) => {
+          return (
+            <ChatMessage 
+              message={chatMessage.message}
+              sender={chatMessage.sender}
+              key={chatMessage.id}
+              time={chatMessage.time}
+            />
+          );
+        })}
+    </div>
+  )
+}
+
+export default ChatMessages;
