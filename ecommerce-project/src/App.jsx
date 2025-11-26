bfd56110a04d4257d33cef840ede1d027512af06
Abhinav <Abhinav.42.Singh@gmail.com>
2025-11-25 19:27:11 -0800

    Generate the HTML for the home page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,9 +2,9 @@ import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
-import './App.css'
 import { TrackingPage } from './pages/TrackingPage'
 import { NotFoundPage } from './pages/NotFoundPage'
+import './App.css'
 
 function App() {
   return (
