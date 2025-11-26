28d45a78ee2a5a4f3e80cddcb71d5226a5bad061
Abhinav <Abhinav.42.Singh@gmail.com>
2025-11-25 19:45:44 -0800

    Fetch data from the backend and log

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
 import { Header } from '../components/Header';
-import CheckmarkIcon from '../assets/images/icons/checkmark.png'
 import { products } from '../../starting-code/data/products'
+import CheckmarkIcon from '../assets/images/icons/checkmark.png'
 import './HomePage.css'
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+        console.log(data);
+    });
+
   return (
     <>
       <title>Ecommerce Project</title>
