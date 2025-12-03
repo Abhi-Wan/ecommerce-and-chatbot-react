118ae8baeca81216a09177b6035762675ae320cc
Abhinav <Abhinav.42.Singh@gmail.com>
2025-12-02 20:58:32 -0800

    Create integration tests for PaymentSummary component

---
+++ ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
@@ -0,0 +1,82 @@
+import { it, expect, describe, vi, beforeEach } from 'vitest';
+import { render, screen, within } from '@testing-library/react';
+import { MemoryRouter, useLocation } from 'react-router';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
+import { PaymentSummary } from './PaymentSummary';
+
+vi.mock('axios');
+
+describe('PaymentSummary component', () => {
+  let paymentSummary;
+  let loadCart;
+  let user;
+
+  beforeEach(() => {
+    paymentSummary = {
+      totalItems: 10,
+      productCostCents: 14601,
+      shippingCostCents: 499,
+      totalCostBeforeTaxCents: 15100,
+      taxCents: 1510,
+      totalCostCents: 16610
+    };
+
+    loadCart = vi.fn();
+
+    user = userEvent.setup();
+  });
+
+  it('displays the correct details', () => {
+    render(
+      <MemoryRouter>
+        <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('Items (10):')).toBeInTheDocument();
+
+    expect(
+      within(screen.getByTestId('payment-summary-product-cost'))
+        .getByText('$146.01')
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByTestId('payment-summary-shipping-cost')
+    ).toHaveTextContent('$4.99');
+
+    expect(
+      screen.getByTestId('payment-summary-total-before-tax')
+    ).toHaveTextContent('$151.00');
+
+    expect(
+      screen.getByTestId('payment-summary-tax')
+    ).toHaveTextContent('$15.10');
+
+    expect(
+      screen.getByTestId('payment-summary-total')
+    ).toHaveTextContent('$166.10');
+  });
+
+  it('places an order', async () => {
+    function Location() {
+      const location = useLocation();
+      return <div data-testid="url-path">{location.pathname}</div>;
+    }
+
+    render(
+      <MemoryRouter>
+        <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
+        <Location />
+      </MemoryRouter>
+    );
+
+    const placeOrderButton = screen.getByTestId('place-order-button');
+    await user.click(placeOrderButton);
+
+    expect(axios.post).toHaveBeenCalledWith('/api/orders');
+    expect(loadCart).toHaveBeenCalled();
+
+    expect(screen.getByTestId('url-path')).toHaveTextContent('/orders');
+  });
+});
